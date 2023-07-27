61e2e0990cd4e482d164f836354863b6d30e6f45
DevKDuck <loading95@naver.com>
2023-07-27 16:13:54 +0900

    Papago API 번역 URLSession ->  Alamofire 변경

--- ChuckNorrisJoke/ViewController.swift
+++ ChuckNorrisJoke/ViewController.swift
@@ -81,57 +81,93 @@ class ViewController: UIViewController {
     }
     
     @IBAction func translateButton(_ sender: UIButton) {
-        change(str: jokeLabel.text ?? "")
+//        change(str: jokeLabel.text ?? "")
+        
+        translationFromEnglishToKorean(str: jokeLabel.text ?? "")
     }
     
-    func change(str: String){
+    
+    //MARK: Papago API URLSession을 이용하여 번역
+//    func change(str: String){
+//        let url = "https://openapi.naver.com/v1/papago/n2mt"
+//
+//
+//        guard let url = URL(string: url) else {
+//            print("Error: cannot create URL")
+//            return
+//        }
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//        request.addValue("application/x-www-form-urlencoded; charset=UTF-8", forHTTPHeaderField: "Content-Type")
+//        request.addValue(Storage().naverClientID, forHTTPHeaderField: "X-Naver-Client-Id")
+//        request.addValue(Storage().naverClientSecret, forHTTPHeaderField: "X-Naver-Client-Secret")
+//
+//        let param = "source=en&target=ko&text=\(str)"
+//
+//        request.httpBody = param.data(using: .utf8)
+//
+//        URLSession.shared.dataTask(with: request){ data, response, error in
+//            guard error == nil else{
+//                print("Error: error calling GET")
+//                return
+//            }
+//            guard let data = data else{
+//                print("Error: Did not recieve data")
+//                return
+//            }
+//
+//
+//            guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
+//                if let res = response as? HTTPURLResponse, (400 ..< 500) ~= res.statusCode{
+//                    print(res.statusCode.description)
+//                }
+//
+//                print("Error: HTTP request failed")
+//                return
+//            }
+//            guard let output = try? JSONDecoder().decode(PaPago.self,from: data) else{
+//                print("PaPago JSON Parsing failed")
+//                return
+//            }
+//
+//            DispatchQueue.main.async {
+//                self.jokeLabelKOR.text = output.message.result.translatedText
+//            }
+//
+//        }.resume()
+//
+//
+//    }
+    
+    func translationFromEnglishToKorean(str: String){
         let url = "https://openapi.naver.com/v1/papago/n2mt"
-        
-        
         guard let url = URL(string: url) else {
             print("Error: cannot create URL")
             return
         }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/x-www-form-urlencoded; charset=UTF-8", forHTTPHeaderField: "Content-Type")
-        request.addValue(Storage().naverClientID, forHTTPHeaderField: "X-Naver-Client-Id")
-        request.addValue(Storage().naverClientSecret, forHTTPHeaderField: "X-Naver-Client-Secret")
+        let param: Parameters = ["source" : "en",
+                                 "target" : "ko",
+                                 "text" : str]
         
-        let param = "source=en&target=ko&text=\(str)"
         
-        request.httpBody = param.data(using: .utf8)
-        
-        URLSession.shared.dataTask(with: request){ data, response, error in
-            guard error == nil else{
-                print("Error: error calling GET")
-                return
-            }
-            guard let data = data else{
-                print("Error: Did not recieve data")
-                return
-            }
-            
-            
-            guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
-                if let res = response as? HTTPURLResponse, (400 ..< 500) ~= res.statusCode{
-                    print(res.statusCode.description)
+        AF.request(url, method: .post,
+                   parameters: param,
+                   encoding: URLEncoding.default,
+                   headers: ["Content-Type" : "application/x-www-form-urlencoded; charset=UTF-8",
+                             "X-Naver-Client-Id" : Storage().naverClientID, "X-Naver-Client-Secret" : Storage().naverClientSecret])
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: PaPago.self){ response in
+                switch response.result{
+                case.success(let data):
+                    DispatchQueue.main.async {
+                        self.jokeLabelKOR.text = data.message.result.translatedText
+                    }
+                case.failure(_):
+                    print("Papago API POST failed")
                 }
                 
-                print("Error: HTTP request failed")
-                return
-            }
-            guard let output = try? JSONDecoder().decode(PaPago.self,from: data) else{
-                print("PaPago JSON Parsing failed")
-                return
-            }
-                
-            DispatchQueue.main.async {
-                self.jokeLabelKOR.text = output.message.result.translatedText
             }
             
-        }.resume()
-        
         
     }
     
